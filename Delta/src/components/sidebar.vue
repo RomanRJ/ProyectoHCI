ddd2fd4d1888f4f0475f65b79b58ffd1057d0087
RomanRJ <rjdilli84@gmail.com>
2023-10-18 20:37:00 -0600

    imagenes ya cargadas

--- Delta/src/components/sidebar.vue
+++ Delta/src/components/sidebar.vue
@@ -2,13 +2,12 @@
     <div class="contenedor">
         <header>
             <div class="logo">
-                <img src="img/logo.png" alt="logo delta">
+                <img src="./img/logo.png" alt="logo delta">
             </div>
         </header>
         <div class="lateral">
             <div class="sidebar">
                 <nav class="menuLaterla">
-                    <a class="boton" @click="Inicio">Inicio</a>
                     <a class="boton" @click="Test">Test</a>
                     <a class="boton" @click="Actividad">Actividad</a>
                     <a class="boton" @click="Resultados">Resultados</a>
@@ -26,7 +25,7 @@
                  Aquí, a través de actividades diseñadas, desarrollaremos tu enfoque, 
                  abordando aspectos clave de la atención. ¡Juntos, fortaleceremos tu capacidad de concentración!</p>
                  <div class="thumb">
-                    <img src="img/1.png" alt="paz">
+                    <img src="./img/1.png" alt="paz">
                 </div>
             <p>Las actividades prácticas te ofrecerán una experiencia directa para fortalecer tu
                  enfoque y avanzar hacia una mejor concentración rápidamente. ¡Comencemos esta 
@@ -46,7 +45,7 @@
             </div>
             <div class="cuadro-pequeno">
                 <div class="thumb">
-                <img src="img/2.jpg" alt="ciudad">
+                <img src="./img/2.jpg" alt="ciudad">
                 </div>
             </div>
         </div>
@@ -133,6 +132,10 @@ header {
     text-align: center;
     display: block;
     margin: 40px 8px;
+}
+.boton:hover{
+    cursor: pointer;
+    opacity: 0.7;
 }
 .main {
     font-family: 'Open Sans', sans-serif;
